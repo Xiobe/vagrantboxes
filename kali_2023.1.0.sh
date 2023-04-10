3569aafd3161457849447180c0853fe031e8441d
Xiobe <Xiobe@users.noreply.github.com>
2023-04-10 11:20:37 +0200

    Bug fix
    Missing box argument, rename of the box, removal of previous kali boxes and adding echo in order to have clearer output.

--- kali_2023.1.0.sh
+++ kali_2023.1.0.sh
@@ -1,2 +1,6 @@
-#! /usr/bin/env bash                                                                                                                        
-vagrant add kali_2023.1.0 https://app.vagrantup.com/kalilinux/boxes/rolling/versions/2023.1.0/providers/virtualbox.box
+#! /usr/bin/env bash
+echo Removing existing kali box from your vagrant environment
+vagrant box remove kali
+
+echo Adding kali to your vagrant environment
+vagrant box add kali https://app.vagrantup.com/kalilinux/boxes/rolling/versions/2023.1.0/providers/virtualbox.box
