f086f970f571f1f2c2ddd44cad22be56bb734a56
Xiobe <Xiobe@users.noreply.github.com>
2023-04-10 11:50:07 +0200

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,7 +8,7 @@ echo Adding Ubuntu Lunar Lobster (23.04) to your vagrant environment
 vagrant box add ubuntu-lunar https://app.vagrantup.com/ubuntu/boxes/lunar64/versions/20230301.0.0/providers/virtualbox.box
 
 # Ubuntu Jammy Jellyfish (22.04, LTS)
-echo Removing existing Ubuntu Jammy Jellyfish (22.04, LTS) from your vagrant environment
+echo Removing existing Ubuntu Jammy Jellyfish (22.04) LTS from your vagrant environment
 vagrant box remove ubuntu-lts
-echo Adding Ubuntu Jammy Jellyfish (22.04, LTS) to your vagrant environment
+echo Adding Ubuntu Jammy Jellyfish (22.04) LTS to your vagrant environment
 vagrant box add ubuntu-lts https://app.vagrantup.com/ubuntu/boxes/jammy64/versions/20230314.0.0/providers/virtualbox.box
