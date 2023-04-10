df059dff13a79ff7b80c88d3cf911c42892c8dd4
Xiobe <Xiobe@users.noreply.github.com>
2023-04-10 11:10:56 +0200

    Create kali_2023.1.0.sh

---
+++ kali_2023.1.0.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash                                                                                                                        
+vagrant add kali_2023.1.0 https://app.vagrantup.com/kalilinux/boxes/rolling/versions/2023.1.0/providers/virtualbox.box
